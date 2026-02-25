bf3cad4a39dac7758bbf8fb1cff6597207f19960
rdpresser <rodrigo.presser@gmail.com>
2026-02-25 11:37:21 -0300

    Remove unused methods from IAlertHubClient interface
    AlertAcknowledged and AlertResolved methods were removed from the IAlertHubClient interface, leaving only AlertCreated. This streamlines the interface to only include currently needed notifications.

--- src/Adapters/Inbound/TC.Agro.Analytics.Service/Hubs/IAlertHubClient.cs
+++ src/Adapters/Inbound/TC.Agro.Analytics.Service/Hubs/IAlertHubClient.cs
@@ -3,8 +3,6 @@ namespace TC.Agro.Analytics.Service.Hubs;
 public interface IAlertHubClient
 {
     Task AlertCreated(AlertCreatedNotification alert);
-
     Task AlertAcknowledged(AlertAcknowledgedNotification alert);
-
     Task AlertResolved(AlertResolvedNotification alert);
 }
