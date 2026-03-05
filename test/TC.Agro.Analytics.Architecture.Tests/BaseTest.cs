bab4073a7560d948dfd88b89e92c66effa6370af
rdpresser <rodrigo.presser@gmail.com>
2026-03-05 12:40:22 -0300

    Add architecture tests to enforce layer boundaries
    Added TC.Agro.Analytics.Architecture.Tests project to the solution, using NetArchTest.Rules to verify clean architecture boundaries between Domain, Application, Infrastructure, and Presentation layers. Included tests to ensure no forbidden dependencies exist between layers. Updated packages and solution file accordingly.

---
+++ test/TC.Agro.Analytics.Architecture.Tests/BaseTest.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using TC.Agro.Analytics.Application.UseCases.Alerts.GetPendingAlerts;
+using TC.Agro.Analytics.Domain.Aggregates;
+using TC.Agro.Analytics.Infrastructure;
+using TC.Agro.Analytics.Service;
+
+namespace TC.Agro.Analytics.Architecture.Tests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(AlertAggregate).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(GetPendingAlertsQuery).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
